91d3c86f790d9b254db310ce861ad4a98afbd125
Jansen Rodriguez <JansenR125@gmail.com>
2021-01-18 17:46:25 -0500

    Using queries to retrieve objects

---
+++ com/luv2code/hibernate/demo/QueryStudentDemo.java
@@ -0,0 +1,52 @@
+package com.luv2code.hibernate.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class QueryStudentDemo {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration()
+								 .configure("hibernate.cfg.xml")
+								 .addAnnotatedClass(Student.class)
+								 .buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// Start transaction
+			session.beginTransaction();
+			
+			// Query students
+			List<Student> theStudents = session.createQuery("from Student").getResultList();
+			
+			// Display the students
+			for (Student tempStudent : theStudents) {
+				System.out.println(tempStudent);
+			}
+			
+			// Find students where lastName = "Doe"
+			theStudents = session.createQuery("from Student where lastName='Doe'").getResultList();
+			
+			System.out.println("\n\nStudents with last name of Doe");
+			for(Student tempStudent : theStudents) {
+				System.out.println(tempStudent);
+			}
+			
+			// Commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+}
